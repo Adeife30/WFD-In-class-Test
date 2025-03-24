d547d2a16eb5ced67b1abe4a18b56e75f74675c2
Adeife30 <B00154702@mytudublin.ie>
2025-03-24 15:10:50 +0000

    Add Part A and Part B for class test

---
+++ PartA.py
@@ -0,0 +1,90 @@
+# File: ClassTest/PartA.py
+
+from datetime import date
+
+class Staff:
+    def __init__(self, name: str, DoB: str, sex: str, staffID: int, address: str):
+        self.name = name
+        self.DoB = DoB
+        self.sex = sex
+        self.staffID = staffID
+        self.address = address
+
+    def print_attributes(self):
+        print(f"Name: {self.name}, DoB: {self.DoB}, Sex: {self.sex}, Staff ID: {self.staffID}, Address: {self.address}")
+
+    def update_name(self, name):
+        if isinstance(name, str):
+            self.name = name
+        else:
+            print("Invalid type for name")
+
+    def update_DoB(self, DoB):
+        if isinstance(DoB, str):
+            self.DoB = DoB
+        else:
+            print("Invalid type for DoB")
+
+    def update_sex(self, sex):
+        if isinstance(sex, str):
+            self.sex = sex
+        else:
+            print("Invalid type for sex")
+
+    def update_staffID(self, staffID):
+        if isinstance(staffID, int):
+            self.staffID = staffID
+        else:
+            print("Invalid type for staffID")
+
+    def update_address(self, address):
+        if isinstance(address, str):
+            self.address = address
+        else:
+            print("Invalid type for address")
+
+
+# Child class: FullTimeStaff
+class FullTimeStaff(Staff):
+    def __init__(self, name, DoB, sex, staffID, address, salary: float, department: str):
+        super().__init__(name, DoB, sex, staffID, address)
+        self.salary = salary
+        self.department = department
+
+    def print_all_attributes(self):
+        self.print_attributes()
+        print(f"Salary: {self.salary}, Department: {self.department}")
+
+    def update_salary(self, salary):
+        if isinstance(salary, (int, float)):
+            self.salary = salary
+        else:
+            print("Invalid type for salary")
+
+    def update_department(self, department):
+        if isinstance(department, str):
+            self.department = department
+        else:
+            print("Invalid type for department")
+
+
+# A8: Instances
+staff1 = Staff("Adeife Thomas", "09-12-2003", "Male", 4702, "123 Blanchardstown")
+full_staff1 = FullTimeStaff("Chris Rock", "21-06-2003", "Male", 4703, "78 Castlecurragh Park, Blanchardstown", 55000.00, "IT")
+
+# A9: Print attributes
+print("Staff1 attributes:")
+staff1.print_attributes()
+print("\nFullTimeStaff attributes:")
+full_staff1.print_all_attributes()
+
+# A10: Update examples
+print("\nUpdating Staff1's name and address...")
+staff1.update_name("Adeife Thomas")
+staff1.update_address( "123 Blanchardstown")
+staff1.print_attributes()
+
+print("\nUpdating FullTimeStaff's salary and department...")
+full_staff1.update_salary(55000.00)
+full_staff1.update_department("IT")
+full_staff1.print_all_attributes()
---
+++ PartB.py
@@ -0,0 +1,63 @@
+
+
+import unittest
+from PartA import Staff, FullTimeStaff
+
+class TestStaffClasses(unittest.TestCase):
+
+    def setUp(self):
+        self.staff = Staff("Adeife Thomas", "09-12-2003", "Male", 4702, "123 Blanchardstown")
+        self.full_staff = FullTimeStaff("Chris Rock", "21-06-2003", "Male", 4703, "78 Castlecurragh Park, Blanchardstown", 55000.00, "IT")
+
+    # B2: Test if object is instance of class
+    def test_instance_of_staff(self):
+        self.assertIsInstance(self.staff, Staff)
+        self.assertIsInstance(self.full_staff, FullTimeStaff)
+
+    #Test if object is NOT instance of a class
+    def test_not_instance(self):
+        self.assertNotIsInstance(self.staff, FullTimeStaff)
+        self.assertNotIsInstance(self.full_staff, dict)
+
+    #Test if 2 objects are identical
+    def test_object_identity(self):
+        another_staff = self.staff
+        self.assertIs(self.staff, another_staff)
+        self.assertIsNot(self.staff, self.full_staff)
+
+    #Update methods for Staff class
+    def test_update_name(self):
+        self.staff.update_name("Adeife Thomas")  
+        self.assertEqual(self.staff.name, "Adeife Thomas")
+
+    def test_update_DoB(self):
+        self.staff.update_DoB("09-12-2003")
+        self.assertEqual(self.staff.DoB, "09-12-2003")
+
+    def test_update_sex(self):
+        self.staff.update_sex("Male")
+        self.assertEqual(self.staff.sex, "Male")
+
+    def test_update_staffID(self):
+        self.staff.update_staffID(9999)
+        self.assertEqual(self.staff.staffID, 9999)
+
+
+    def test_update_address(self):
+        self.staff.update_address("123 Blanchardstown") 
+        self.assertEqual(self.staff.address, "123 Blanchardstown")
+
+    # Update methods for FullTimeStaff class
+    def test_update_salary(self):
+        self.full_staff.update_salary(55000.00)  
+        self.assertEqual(self.full_staff.salary, 55000.00)
+
+    def test_update_department(self):
+        self.full_staff.update_department("IT")  
+        self.assertEqual(self.full_staff.department, "IT")
+
+
+#Run tests
+if __name__ == '__main__':
+    unittest.main()
+
